c91721d9275f9c3326d1f4e526832d4e0eef9265
Wending Li <155922688+li-wending@users.noreply.github.com>
2025-07-10 18:27:05 -0400

    add comments to obtain license for CIBERSORT.R

--- R/CIBERSORT.R
+++ R/CIBERSORT.R
@@ -1,1 +1,13 @@
 
+# CIBERSORT prerequisite
+CIBERSORT is free of charge for non-commercial use only. 
+Following registration, the CIBERSORT source code is available via the 
+[cibersort website](https://cibersortx.stanford.edu/csdownload.php),
+and download the CIBERSORT source code.
+
+The source code contains the following file which is
+essential for the successful execution of miR-TS:
+
+```
+CIBERSORT.R
+```
