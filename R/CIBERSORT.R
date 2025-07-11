000176c645c91853a580563468eadffbbaedf56f
Wending Li <155922688+li-wending@users.noreply.github.com>
2025-07-11 19:06:07 -0400

    Update CIBERSORT.R

--- R/CIBERSORT.R
+++ R/CIBERSORT.R
@@ -1,12 +1,10 @@
+## 1. CIBERSORT prerequisite ####
+#' miR-TS is optimized from the CIBERSORT code.
+#' CIBERSORT is licensed but free of charge for non-commercial use only.
+#' Following registration, the CIBERSORT source code is available via the
+#' [cibersort website](https://cibersortx.stanford.edu/).
+#' The function in the following file is essential for
+#' the successful execution of miR-TS:
+#' "CIBERSORT.R"
 
-# CIBERSORT prerequisite
-CIBERSORT is free of charge for non-commercial use only. 
-Following registration, the CIBERSORT source code is available via the 
-[cibersort website](https://cibersortx.stanford.edu/csdownload.php).
-
-The source code contains the following file which is
-essential for the successful execution of miR-TS:
-
-```
-CIBERSORT.R
-```
+print("Please go to: https://cibersortx.stanford.edu/ to download and replace the CIBERSORT.R in this folder.")
